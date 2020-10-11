e69bf2a9cc56ef923cae36ab80a1502c06f19777
Dana Pham <danapham12@gmail.com>
2020-10-11 12:54:41 -0500

    preset values show up

--- src/javascripts/components/forms/editPinForm.js
+++ src/javascripts/components/forms/editPinForm.js
@@ -1,36 +1,33 @@
 import boardData from '../../helpers/data/boardData';
-import pinData from '../../helpers/data/pinData';
 
-const editPinForm = (pinId) => {
-  boardData.getAllBoards().then((response) => {
-    const boardTitles = [];
-    $('#selectedBoardTitle').html('');
-    $('#selectedBoardTitle').append('<option selected>Select a board</option>');
-    response.forEach((board) => {
-      $('#selectedBoardTitle').append(`<option value="${board.boardId}" id="">${board.title}</option>`);
-      boardTitles.push(board.title);
-    });
-  }).catch((error) => console.warn(error));
-
-  pinData.getSinglePin(pinId);
-
+const editPinForm = (singlePin) => {
   $('#editPinForm').html(`
   <div id="pinSuccessMsg"></div>
   <form>
   <div id="pinErrorMsg"></div>
   <div class="form-group">
     <label for="imageUrl">Image Url</label>
-    <input type="url" class="form-control" id="imageUrl">
+    <input type="url" class="form-control" id="imageUrl" value="${singlePin.imageUrl}">
   </div>
   <div class="form-group">
     <label for="caption">Caption</label>
-    <input type="text" class="form-control" id="caption">
+    <input type="text" class="form-control" id="caption" value="${singlePin.caption}">
   </div>
   <select class="custom-select" id="selectedBoardTitle">
   </select>
   <button type="submit" id="submitPin" class="btn btn-primary">Submit</button>
 </form>
 `);
+
+  boardData.getAllBoards().then((response) => {
+    // const boardTitles = [];
+    $('#selectedBoardTitle').html('');
+    $('#selectedBoardTitle').append('<option>Select a board</option>');
+    response.forEach((board) => {
+      $('#selectedBoardTitle').append(`<option value="${board.boardId}" ${board.boardId === singlePin.boardId ? 'selected' : ''}>${board.title}</option>`);
+      // boardTitles.push(board.title);
+    });
+  }).catch((error) => console.warn(error));
 };
 
 export default { editPinForm };
--- src/javascripts/components/views/editPin.js
+++ src/javascripts/components/views/editPin.js
@@ -1,8 +1,12 @@
 import editPinForm from '../forms/editPinForm';
+import pinData from '../../helpers/data/pinData';
 
 const editPinView = (pinId) => {
   $('#app').html('<div id="editPinForm"></div>');
-  editPinForm.editPinForm(pinId);
+
+  pinData.getSinglePin(pinId).then((response) => {
+    editPinForm.editPinForm(response);
+  }).catch((error) => console.warn(error));
 };
 
 export default { editPinView };
