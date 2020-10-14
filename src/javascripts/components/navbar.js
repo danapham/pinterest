3b62de8aa536f3eed5c16b89d4e36f27199e7534
Dana Pham <danapham12@gmail.com>
2020-10-14 16:09:50 -0500

    make navbar collapse when in smaller view

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -11,7 +11,10 @@ const displayUserNav = () => {
   $('#nav').html(`
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Savorites</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav user-view">
         <li class="nav-item active" id="boardsLink">
           <a class="nav-link" href="#">Boards <span class="sr-only">(current)</span></a>
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,7 @@ import '../styles/main.scss';
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
+import 'bootstrap';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
